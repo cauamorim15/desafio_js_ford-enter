e4d4f88e17d434c4de162fc0e776f6add06b0ed4
cauamorim15 <amorim.santos231527@gmail.com>
2026-08-10 15:09:31 -0300

    comparação e inicio contato

--- js/compare.js
+++ js/compare.js
@@ -6,7 +6,18 @@ class Car {
    
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga =capacidadeCarga;
+        this.motor = motor;
+        this.potencia= potencia;
+        this.volumeCacamba =volumeCacamba;
+        this.roda = roda; 
+        this.image = image
+
     }
 } 
 
@@ -20,19 +31,22 @@ function GetCarArrPosition(arr, carClass) {
 }
 
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
+        
+        if(el.checked && carArr.length >=2){
+               alert('Só pode marcar 2 carros ao mesmo tempo!') ;
+                el.checked = false;
+                return;
+        }
         if(el.checked){
-                
-            
-        } else {
-          
-        } 
-    } else {
-        throw "You need set a Car Class";
+            carArr.push(carClass);
+        }else{
+            const index = GetCarArrPosition(carArr,carClass);
+            if(index !== -1){
+                carArr.splice(index,1);
+            }
+        }
     }
-}
-
+  
 function ShowCompare() {
     if(carArr.length < 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
@@ -48,5 +62,48 @@ function HideCompare(){
 }
 
 function UpdateCompareTable() {
+
+    const compare0 = carArr[0];
+    const compare1 = carArr[1];
+
+    document.querySelector("#compare_image_0").innerHTML = `<img src="${compare0.image}">`;
+    document.querySelector("#compare_image_1").innerHTML = `<img src="${compare1.image}">`;
+
+    document.querySelector("#compare_modelo_0").innerHTML = compare0.nome;
+    document.querySelector("#compare_modelo_1").innerHTML = compare1.nome;
+
+    document.querySelector("#compare_alturacacamba_0").innerHTML =compare0.alturaCacamba;
+    document.querySelector("#compare_alturacacamba_1").innerHTML =compare1.alturaCacamba;
+
+    document.querySelector("#compare_alturaveiculo_0").innerHTML = compare0.alturaVeiculo;
+    document.querySelector("#compare_alturaveiculo_1").innerHTML = compare1.alturaVeiculo;
+
+    document.querySelector("#compare_alturasolo_0").innerHTML = compare0.alturaSolo;
+    document.querySelector("#compare_alturasolo_1").innerHTML = compare1.alturaSolo;
+
+    document.querySelector("#compare_capacidadecarga_0").innerHTML = compare0.capacidadeCarga;
+    document.querySelector("#compare_capacidadecarga_1").innerHTML = compare1.capacidadeCarga;
+
+    document.querySelector("#compare_motor_0").innerHTML=compare0.motor;
+    document.querySelector("#compare_motor_1").innerHTML=compare1.motor;
+
+    document.querySelector("#compare_potencia_0").innerHTML = compare0.potencia;
+    document.querySelector("#compare_potencia_1").innerHTML = compare1.potencia;
     
+
+    document.querySelector("#compare_volumecacamba_0").innerHTML = compare0.volumeCacamba;
+    document.querySelector("#compare_volumecacamba_1").innerHTML = compare1.volumeCacamba;
+
+    document.querySelector("#compare_roda_0").innerHTML = compare0.roda;
+    document.querySelector("#compare_roda_1").innerHTML = compare1.roda;
+
+
+   document.querySelector("#compare_preco_0").innerHTML = (compare0.preco / 1).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    document.querySelector("#compare_preco_1").innerHTML = (compare1.preco / 1).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+
+
+
+
+
 }
--- js/form.js
+++ js/form.js
@@ -1,18 +1,28 @@
 
 //class contato
 
-class contato {
-    
+class Contato {
+  constructor(nome,email, cpf, sobrenome,telefone, contato) {
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
+    this.sobrenome = sobrenome;
+    this.telefone = telefone;
+    this.contato = contato;
+  }
 }
 
 function Post(form) {
-
-  let data = new contato(form.elements.namedItem("nome").value,
+  let data = new Contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
+
+console.log(data);
+
+Enviar();
   
 }
 
@@ -22,6 +32,8 @@ function Enviar() {
 
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }else{
+        alert(`Tente novamente! `);
     }
 
 }
