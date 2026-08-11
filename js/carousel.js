f7fef45406a36cd7a1ce4111c8d495fa162c1a5d
cauamorim15 <amorim.santos231527@gmail.com>
2026-08-11 11:29:46 -0300

    projeto final

--- js/carousel.js
+++ js/carousel.js
@@ -8,7 +8,12 @@ let carouselArr = [];
 
 //class Carousel
 class Carousel {
+ constructor(imagem, texto, link){
+    this.imagem = imagem;
+    this.texto = texto;
+    this.link = link; 
 
+ }
     
       
     static Start(arr){
@@ -17,8 +22,9 @@ class Carousel {
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
+                Carousel._arr = arr;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },3000);
             }
             
         } else {
@@ -26,7 +32,42 @@ class Carousel {
         }
     }
 
+
+
+
     static Next(){
+        let inicializacao = carouselArr[Carousel._sequence];
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
+        
+        let carrosselDiv = document.getElementById("carousel");
+        let titulo = document.getElementById("carousel-title");
+
+        carrosselDiv.innerHTML = `<img src="${inicializacao.imagem} ">  `
+
+        titulo.innerHTML = `<a href ="${inicializacao.link}">${inicializacao.texto}</a>`
+
+        Carousel._sequence++;
+
+        
         
     }
+    static voltar(){
+        Carousel._sequence -= 2;
+
+        if(Carousel._sequence <0 ){
+            Carousel._sequence += Carousel._size;
+        }
+        Carousel.Next();
+    };
+
 };
+
+function passar() {
+        Carousel.Next();
+    };
+function voltar() {
+        Carousel.voltar();
+    }
+
